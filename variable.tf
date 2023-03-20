f9f48c28de1d0fc07d78f6aaae744103501f73b0
elif-admin <elifmalkoc55@gmail.com>
2023-03-20 22:21:31 +0000

    added

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "git-name" {
+  default = "elif-admin"
+}
+variable "git-token" {
+  default = "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"
+}
+variable "key-name" {
+  default = "first-key"
+}
+variable "hosted-zone" {
+  default = "elifdevops.co.uk."
+}
